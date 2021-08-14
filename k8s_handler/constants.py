8762c14ea6b9eae6e436859513fd4aea9e03b2f1
LiuJianwen219 <jianwen_liu219@163.com>
2021-08-14 20:43:07 +0800

    feat: python kubernetes client try success, with some constants values.

---
+++ k8s_handler/constants.py
@@ -0,0 +1,17 @@
+image = "wxwd14388/vclient:v1.0.2"
+
+compileScript = "compile.sh"
+
+vivadoPath = "/tools"
+
+vivado = "/tools/Xilinx/Vivado/2020.1/bin/vivado"
+
+FPGAVersion = "xc7k160tffg676-2L"
+
+inContainerSourceRoot = "/FrexTSource"
+
+main_tcl = "main.tcl"
+
+compileThread = "4"
+
+ttl_seconds_after_finished = 100
--- k8s_handler/k8s_handler.py
+++ k8s_handler/k8s_handler.py
@@ -1,6 +1,156 @@
-from kubernetes import client, config
+import os
+import sys
 
+sys.path.append('/FrexT/k8s_handler')  # 导入test文件的绝对路径
+
+from kubernetes import client, config
+import uuid
 from time import sleep
+from k8s_handler import constants as const
+from compiler import constants as c_const
+
+
+class k8s_handler:
+    # "/tmp/FrexT"
+    def __init__(self, tempPath):
+        # Configs can be set in Configuration class directly or using helper
+        # utility. If no argument provided, the config will be loaded from
+        # default location.
+        config.load_incluster_config()
+        self.api_instance = client.BatchV1Api()
+        self.tempPath = tempPath
+        self.jobName = None
+        self.namespace = "default"
+
+    # Create a job object with client-python API. The job we
+    # created is same as the `pi-job.yaml` in the /examples folder.
+    # "mod60", "topMod60"
+    def create_job_object(self, topic, topModuleName):
+        # volumes mount to pod
+        volume_mount_vivado = client.V1VolumeMount(
+            name="vivado",
+            mount_path=const.vivadoPath,
+        )
+        volume_mount_source = client.V1VolumeMount(
+            name="source",
+            mount_path=const.inContainerSourceRoot
+        )
+
+        # volume definitions
+        volume_vivado = client.V1Volume(
+            name="vivado",
+            host_path=client.V1HostPathVolumeSource(
+                path=const.vivadoPath,
+                type="Directory",
+            )
+        )
+        volume_source = client.V1Volume(
+            name="source",
+            host_path=client.V1HostPathVolumeSource(
+                path=self.tempPath,
+                type="Directory",
+            )
+        )
+
+        # Configureate Pod template container
+        container = client.V1Container(
+            name="cmp-job-" + uuid.uuid1().__str__(),
+            image=const.image,
+            command=["/bin/sh"],
+            args=[
+                const.compileScript,
+                os.path.join(const.inContainerSourceRoot, topic + c_const.questions_suffix),
+                os.path.join(const.inContainerSourceRoot, topic),
+                const.vivado,  # vivado.exe dir
+                os.path.join(const.inContainerSourceRoot, const.main_tcl),
+                const.FPGAVersion,
+                const.inContainerSourceRoot,
+                topModuleName,
+                const.compileThread,
+            ],
+            volume_mounts=[
+                volume_mount_vivado,
+                volume_mount_source
+            ],
+        )
+
+        # Create and configurate a spec section
+        template = client.V1PodTemplateSpec(
+            metadata=client.V1ObjectMeta(),
+            spec=client.V1PodSpec(
+                restart_policy="Never",
+                containers=[container],
+                volumes=[
+                    volume_vivado,
+                    volume_source,
+                ],
+            ),
+        )
+
+        # Create the specification of deployment
+        spec = client.V1JobSpec(
+            template=template,
+            backoff_limit=4,
+            ttl_seconds_after_finished=const.ttl_seconds_after_finished,
+        )
+
+        # Instantiate the job object
+        self.jobName = "cmp-" + uuid.uuid1().__str__()
+        job = client.V1Job(
+            api_version="batch/v1",
+            kind="Job",
+            metadata=client.V1ObjectMeta(
+                name=self.jobName
+            ),
+            spec=spec,
+        )
+
+        return job
+
+    def create_job(self, job):
+        api_response = self.api_instance.create_namespaced_job(
+            body=job,
+            namespace=self.namespace,
+        )
+        print("Job created. status='%s'" % str(api_response.status))
+        # Job created. status='{'active': None,
+        #  'completion_time': None,
+        #  'conditions': None,
+        #  'failed': None,
+        #  'start_time': None,
+        #  'succeeded': None}'
+        # get_job_status(api_instance)
+
+    def get_job_status(self):
+        job_completed = False
+        while not job_completed:
+            api_response = self.api_instance.read_namespaced_job_status(
+                name=self.jobName,
+                namespace=self.namespace)
+            if api_response.status.succeeded is not None or \
+                    api_response.status.failed is not None:
+                job_completed = True
+            sleep(1)
+            print("Job status='%s'" % str(api_response.status))
+
+    # def update_job(self, job):
+    #     # Update container image
+    #     job.spec.template.spec.containers[0].image = "perl"
+    #     api_response = self.api_instance.patch_namespaced_job(
+    #         name=self.jobName,
+    #         namespace=self.namespace,
+    #         body=job)
+    #     print("Job updated. status='%s'" % str(api_response.status))
+
+    # def delete_job(self):
+    #     api_response = self.api_instance.delete_namespaced_job(
+    #         name=self.jobName,
+    #         namespace=self.namespace,
+    #         body=client.V1DeleteOptions(
+    #             propagation_policy='Foreground',
+    #             grace_period_seconds=5))
+    #     print("Job deleted. status='%s'" % str(api_response.status))
+
 
 def list_pods(namespace):
     config.load_incluster_config()
@@ -13,77 +163,13 @@ def list_pods(namespace):
         print("%s\t%s\t%s" %
               (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
-JOB_NAME = "pi"
 
-def create_job_object():
-    # Configureate Pod template container
-    container = client.V1Container(
-        name="pi",
-        image="perl",
-        command=["perl", "-Mbignum=bpi", "-wle", "print bpi(2000)"]
-    )
-    # Create and configurate a spec section
-    template = client.V1PodTemplateSpec(
-        metadata=client.V1ObjectMeta(labels={"app": "pi"}),
-        spec=client.V1PodSpec(restart_policy="Never", containers=[container])
-    )
-    # Create the specification of deployment
-    spec = client.V1JobSpec(
-        template=template,
-        backoff_limit=4
-    )
-    # Instantiate the job object
-    job = client.V1Job(
-        api_version="batch/v1",
-        kind="Job",
-        metadata=client.V1ObjectMeta(name=JOB_NAME),
-        spec=spec
-    )
-
-    return job
-
-
-def create_job(api_instance, job):
-    api_response = api_instance.create_namespaced_job(
-        body=job,
-        namespace="default")
-    print("Job created. status='%s'" % str(api_response.status))
-    get_job_status(api_instance)
-
-
-def get_job_status(api_instance):
-    job_completed = False
-    while not job_completed:
-        api_response = api_instance.read_namespaced_job_status(
-            name=JOB_NAME,
-            namespace="default")
-        if api_response.status.succeeded is not None or \
-                api_response.status.failed is not None:
-            job_completed = True
-        sleep(1)
-        print("Job status='%s'" % str(api_response.status))
-
-
-def update_job(api_instance, job):
-    # Update container image
-    job.spec.template.spec.containers[0].image = "perl"
-    api_response = api_instance.patch_namespaced_job(
-        name=JOB_NAME,
-        namespace="default",
-        body=job)
-    print("Job updated. status='%s'" % str(api_response.status))
-
-
-def delete_job(api_instance):
-    api_response = api_instance.delete_namespaced_job(
-        name=JOB_NAME,
-        namespace="default",
-        body=client.V1DeleteOptions(
-            propagation_policy='Foreground',
-            grace_period_seconds=5))
-    print("Job deleted. status='%s'" % str(api_response.status))
-
+def test():
+    job_handler = k8s_handler("/tmp/FrexT")
+    job_spec = job_handler.create_job_object("test", "topMod60")
+    job_handler.create_job(job_spec)
 
 
 if __name__ == '__main__':
     list_pods("default")
+    test()
